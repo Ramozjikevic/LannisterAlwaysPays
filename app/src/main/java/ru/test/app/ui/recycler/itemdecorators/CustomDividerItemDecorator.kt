b5eedfad6f356ac72c5375e47fb895c8cc56077c
Vlad <v.a.chikishev@tmn2.etagi.com>
2020-08-30 06:22:40 +0500

    add custom divider decorator

---
+++ app/src/main/java/ru/test/app/ui/recycler/itemdecorators/CustomDividerItemDecorator.kt
@@ -0,0 +1,22 @@
+package ru.test.app.ui.recycler.itemdecorators
+
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.support.v7.widget.RecyclerView
+
+
+class CustomDividerItemDecorator(private val mDivider: Drawable) : RecyclerView.ItemDecoration() {
+    override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        val dividerLeft = parent.paddingLeft
+        val dividerRight = parent.width - parent.paddingRight
+        val childCount = parent.childCount
+        for (i in 0..childCount - 2) {
+            val child = parent.getChildAt(i)
+            val params = child.layoutParams as RecyclerView.LayoutParams
+            val dividerTop = child.bottom + params.bottomMargin
+            val dividerBottom = dividerTop + mDivider.intrinsicHeight
+            mDivider.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom)
+            mDivider.draw(canvas)
+        }
+    }
+}
